fe6e48ebe9ac8f9850cc206887a76b07c644c234
Cristobal Salazar <chris.salza@gmail.com>
2020-10-15 12:49:56 -0400

    added user

--- docker/dbsetup.sql
+++ docker/dbsetup.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE limesurvey_db;
+CREATE USER 'limesurvey_user'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON limesurvey_db.* TO 'limesurvey_user'@'localhost' IDENTIFIED BY 'password';
 FLUSH PRIVILEGES;
